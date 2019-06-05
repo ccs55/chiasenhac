07f9ec55adad0093d6de913d55d1ba457b13d1a1
nguyenthidinh <51009605+nguyenthidinh@users.noreply.github.com>
2019-06-05 21:10:19 +0700

    Update thongtincasy.php

--- web/thongtincasy.php
+++ web/thongtincasy.php
@@ -21,6 +21,8 @@ include 'header4.php';
                         min-height:2000px;
                         height:auto;
                     ">
+                    <link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all">
+							<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
                     
                     <div style="color:white;height:250px;width:1079px;  margin-top: 2cm;background: url('student_image/<?php echo $key['HinhAnh']?>') center center / cover no-repeat;">
                     
@@ -40,29 +42,66 @@ include 'header4.php';
                             </div>
                             <hr style="    margin-top: 24px;border-bottom: 1px solid #111">
                             </div>
-                            <div>
-                            
-                            <section class=""><div class="container"><div class="sub-container no-line-height">
-                            <div class="row"><div class="col-md-8"><ul style="    width: 100%;    list-style: none;
-    display: -webkit-inline-box;
-    position: relative;
-    z-index: 2;">
-                            <li style="     width: 120px;   position: relative;float: left;display: inline-block;"><a role="button" title="Tổng quan" href="/nghe-si/Thuy-Chi/home">Tổng quan</a></li>
-                            <li style="      width: 80px;  position: relative;float: left;display: inline-block;"><a role="button"  title="MV" href="/nghe-si/Thuy-Chi/video">MV</a></li>
-                            <li style="      width: 80px;  position: relative;float: left;display: inline-block;"><a role="button"  title="Album" href="/nghe-si/Thuy-Chi/album">Album</a></li>
-                            <li style="      width: 80px;  position: relative;float: left;display: inline-block;"><a role="button"  title="Bài hát" href="/nghe-si/Thuy-Chi/bai-hat">Bài hát</a></li>
-                            <li style="      width: 80px;  position: relative;float: left;display: inline-block;"><a role="button" onclick="myfunction()" title="Giới thiệu">Giới thiệu</a></li></ul>
-                            </div></div></div></div></section>
-                            
-                            <script>
-function myfunction() {
-document.getElementById("demo").innerHTML =<?php echo json_encode ($key['TieuSu']); ?>;
-}
-</script>
+                            <div class="bs-example bs-example-tabs" role="tabpanel" data-example-id="togglable-tabs">
+                            <ul id="myTab" class="nav nav-tabs" role="tablist">
+								   <li role="presentation" class="active" style="width:120px; text-align:center;margin-left:30px; "><a href="#home" id="home-tab" role="tab" data-toggle="tab" aria-controls="home" aria-expanded="true">Giới Thiệu</a></li>
+								   <li role="presentation" class="" style="margin-left:30px; width:120px;text-align:center;"><a href="#profile" role="tab" id="profile-tab" data-toggle="tab" aria-controls="profile" aria-expanded="false">Bài Hát</a></li>
 
-<div id="demo">
+								</ul>
+                <div id="myTabContent" class="tab-content">
+								  <div role="tabpanel" class="tab-pane fade active in" id="home" aria-labelledby="home-tab">
+									<div class="browse-inner">
+								 	 <!-- /agileits -->
+										<pre style="margin-left:30px;margin-right:30px;background-color:antiquewhite;"><?php echo $key['TieuSu'] ?></pre>
+								
+									
+									
+											<div class="clearfix"> </div>
+										</div>
+								  </div>
+								  <div role="tabpanel" class="tab-pane fade" id="profile" aria-labelledby="profile-tab">
+									   <div class="browse-inner">
+										 <?php
+									
+                  
+                  
+                  $query1 = "
+                  SELECT * FROM baihat,casy WHERE baihat.CaSy=CaSy.TenCS and baihat.CaSy='$id'
+                  ";
+                  $result1 = mysqli_query($connect, $query1);
+                  if(mysqli_num_rows($result) > 0)
+										{
+                  foreach($result1 as $key1){
+                    ?>
+                    <div style="display:grid;">
+                    <a href="header3.php?id=<?php echo $key1['TenBh'];?>" style="text-decoration:none;">
+                    <div id="user"style="margin-left:30px;">
+                    <img width="50" src="student_image/<?php echo $key1['HinhAnh']?>" id="pic"/>&nbsp;<div><div><span style="font-size:16px;color:#111;"><?php echo $key1['TenBh']?></span></div><div><span style="font-size:14px; color:#111;"><?php echo $key1['CaSy']?></span></div></div></div>
+                    
+                    </a>
+                    </div>
+                    <?php
+                  }
+                }
+                  
+                  
+                  ?>
+									
+									
+								
+									<div class="clearfix"> </div>
+								</div>
+								  </div>   
+                  
 
-<div>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/scripts.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.js"></script>
+                            
+
+
                             </body>
 </html>
                             
