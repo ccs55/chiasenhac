80b1c2da3f1ff475a698ca846affc8061d6770b4
nguyenthidinh <51009605+nguyenthidinh@users.noreply.github.com>
2019-06-05 21:05:21 +0700

    Update header2.php

--- web/header2.php
+++ web/header2.php
@@ -88,3 +88,12 @@
 					  </div>
 					  </div>
 					  </div>
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/scripts.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.js"></script>
+                            
+
+
+                            </body>
+</html>
